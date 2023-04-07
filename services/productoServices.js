83f9bffabdb159ab4e2106db89704d53a72b35e8
María Sofía Fregona <94732342+sofifregona@users.noreply.github.com>
2023-04-07 00:56:20 -0300

    Probando

--- services/productoServices.js
+++ services/productoServices.js
@@ -1,6 +1,212 @@
 const listaProductos = async () => {
   try {
     const respuesta = await fetch(
+      "https://sofifregona.github.io/ecommerce_alura_geek/db.json"
+    );
+    const data = await respuesta.json();
+    return data.productos;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const detalleProducto = async (id) => {
+  try {
+    const respuesta = await fetch(
+      `https://sofifregona.github.io/ecommerce_alura_geek/db.json`
+    );
+    const data = await respuesta.json();
+    const productoEncontrado = data.productos.find(
+      (producto) => producto.id === id
+    );
+    return productoEncontrado;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const actualizarStock = async (id, stock) => {
+  try {
+    const respuesta = await fetch(
+      "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json"
+    );
+    const listaProductos = await respuesta.json();
+    const productoEncontrado = listaProductos.productos.find(
+      (producto) => producto.id === id
+    );
+    if (productoEncontrado) {
+      //productoEncontrado.stock = stock;
+      const respuestaPatch = await fetch(
+        "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //body: JSON.stringify(listaProductos),
+          body: JSON.stringify({
+            stock,
+          }),
+        }
+      );
+      console.log(
+        `Nombre del producto con ID ${id} actualizado a ${nuevoNombre}`
+      );
+      return respuestaPatch;
+    } else {
+      console.log(`No se encontró un producto con el ID ${id}`);
+    }
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const crearProducto = async (
+  id,
+  nombre,
+  seccion,
+  descripcion,
+  precio,
+  stock,
+  imagen
+) => {
+  try {
+    const respuestaPost = await fetch(
+      "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        //mode: 'no-cors',
+        //body: JSON.stringify(listaProductos)
+        body: JSON.stringify({
+          id,
+          nombre,
+          seccion,
+          descripcion,
+          precio,
+          stock,
+          imagen,
+        }),
+      }
+    );
+    return respuestaPost;
+
+    /*return await fetch(
+      "https://ecommerce-alurageek-challenge.herokuapp.com/productos",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+          nombre,
+          seccion,
+          descripcion,
+          precio,
+          stock,
+          imagen,
+        }),
+      }
+    );*/
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const actualizarProducto = async (
+  id,
+  nombre,
+  seccion,
+  descripcion,
+  precio,
+  stock,
+  imagen
+) => {
+  try {
+    const respuesta = await fetch(
+      "https://sofifregona.github.io/ecommerce_alura_geek/db.json"
+    );
+    const listaProductos = await respuesta.json();
+    const productoEncontrado = listaProductos.productos.find(
+      (producto) => producto.id === id
+    );
+    if (productoEncontrado) {
+      productoEncontrado.id = id;
+      productoEncontrado.nombre = nombre;
+      productoEncontrado.seccion = seccion;
+      productoEncontrado.descripcion = descripcion;
+      productoEncontrado.precio = precio;
+      productoEncontrado.istock = stock;
+      productoEncontrado.imagen = imagen;
+
+      const respuestaPatch = await fetch(
+        "https://sofifregona.github.io/Sofia-tienda-de-ropa/db.json",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //mode: 'no-cors',
+          //body: JSON.stringify(listaProductos)
+          body: JSON.stringify({
+            productoEncontrado
+          }),
+        }
+      );
+      return respuestaPatch;
+    }
+    /*const respuesta = await fetch(
+      `https://ecommerce-alurageek-challenge.herokuapp.com/productos/${id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre,
+          seccion,
+          descripcion,
+          precio,
+          stock,
+          imagen,
+        }),
+      }
+    );
+    return respuesta;*/
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+const eliminarProducto = async (id) => {
+  try {
+    return await fetch(
+      `https://ecommerce-alurageek-challenge.herokuapp.com/productos/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+export const productoServices = {
+  listaProductos,
+  detalleProducto,
+  actualizarStock,
+  crearProducto,
+  actualizarProducto,
+  eliminarPr
+
+/*
+
+const listaProductos = async () => {
+  try {
+    const respuesta = await fetch(
       "https://ecommerce-alurageek-challenge.herokuapp.com/productos"
     );
     return await respuesta.json();
@@ -127,3 +333,4 @@ export const productoServices = {
   actualizarProducto,
   eliminarProducto,
 };
+*/
